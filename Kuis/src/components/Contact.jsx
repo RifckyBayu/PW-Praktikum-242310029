e4120f9fb15551d3fc0f01ea05fc1b726caa6354
Rifcky Bayu B <242310029@student.ibik.ac.id>
2026-05-08 17:00:32 +0700

    Kuis-1

---
+++ Kuis/src/components/Contact.jsx
@@ -0,0 +1,14 @@
+function Contact() {
+  return (
+    <section className="section" id="contact">
+      <h2>Contact</h2>
+
+      <p>Email : 242310029@student.ibik.ac.id</p>
+      <p>No. Hp : 083824653051 </p>
+      <p>Instagram : @rbbyu_</p>
+      <p>Github : https://github.com/RifckyBayu</p>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ Kuis/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>© 242310029 Rifcky Bayu Bachtriar Teknologi Informasi | AI Engineer</p>
+    </footer>
+  );
+}
+
+export default Footer;
